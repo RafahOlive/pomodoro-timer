ab6cc1030d9cf668166f1a27d2ae352dca284031
Rafael Ribeiro <rafaelribeiro12@live.com>
2023-03-03 17:31:29 -0300

    configurado input das tasks

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,13 +15,27 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" placeholder="De um nome para o seu projeto" />
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="De um nome para o seu projeto"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Portfolio" />
+            <option value="To-do list" />
+            <option value="IMC" />
+            <option value="Projeto 4" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
           <MinutesAmountInput
             type="number"
             id="minutesAmount"
             placeholder="00"
+            step={5}
+            min={5}
+            max={60}
           />
 
           <span>minutos.</span>
